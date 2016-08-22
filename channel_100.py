1dcff702da1761cc87dfdfb3250210d67210346f
gltabi <tabii12@gmail.com>
2016-08-22 14:59:28 -0700

    Add files via upload

---
+++ channel_100.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("irsend SEND_ONCE arris1 KEY_1")
+os.system("irsend SEND_ONCE arris1 KEY_0")
+os.system("irsend SEND_ONCE arris1 KEY_0")
+os.system("irsend SEND_ONCE arris1 KEY_OK")
