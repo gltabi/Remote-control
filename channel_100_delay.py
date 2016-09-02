bc45024b230adb5e0740efa9abe5e3067ed47ebc
gltabi <tabii12@gmail.com>
2016-09-02 16:57:34 -0700

    Add files via upload

---
+++ channel_100_delay.py
@@ -0,0 +1,13 @@
+import time
+import os
+
+while True:
+	#time.sleep(2)
+	os.system("irsend SEND_ONCE ARRIS_N KEY_LAST")
+	time.sleep(1)
+	#os.system("irsend SEND_ONCE ARRIS_N KEY_VOLUMEDOWN")
+	#time.sleep(2)
+	#os.system("irsend SEND_ONCE ARRIS_2 KEY_DOWN")
+	#time.sleep(0.15)
+	#os.system("irsend SEND_ONCE ARRIS_2 KEY_DOWN")
+	#time.sleep(2)
